0bb6d83c60449a9d45c9ecc4eea7fb9be4230311
Yves Vinckier <yves.vinckier@live.fr>
2018-11-30 00:05:22 +0100

    wheel

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import { graphql } from 'gatsby'
+import debounce from 'lodash/debounce'
+import throttle from 'lodash/throttle'
 
 import Slider from '../components/Slider'
-import data from '../data/data'
 import logo from '../images/logo.svg'
 
 import Layout from '../components/layout'
@@ -15,13 +16,45 @@ class IndexPage extends Component {
       posts: this.props.data.allContentfulGallery.edges,
       post: this.props.data.allContentfulGallery.edges[0].node
     }
+  }
+  componentDidMount() {
+    this.mouseWheelListener = throttle(e => this.handleMouseWheel(e), 2000, {
+      leading: true,
+      trailing: false,
+    })
 
+    window.addEventListener('wheel', this.mouseWheelListener, { passive: true })
   }
 
+  handleMouseWheel({ deltaY }) {
+    const currentIndex = this.state.post.index
+    console.log(currentIndex);
+    const newIndex = deltaY > 0 ? currentIndex + 1 : currentIndex - 1
+    this.handleProjectSwitch(newIndex)
+  }
+  handleProjectSwitch = debounce(
+    newIndex => {
+
+      const projectsDataCount = this.state.posts.length - 1
+      let index = newIndex
+
+      if (index > projectsDataCount) {
+        index = 0
+      } else if (index < 0) {
+        index = projectsDataCount
+      }
+
+      this.setState({
+        post: this.props.data.allContentfulGallery.edges[index].node
+      })
+
+    },
+    350,
+    { leading: true, trailing: false }
+  )
 
   nextPost = () => {
     const newIndex = this.state.post.index + 1;
-    console.log(newIndex);
     this.setState({
       post: this.props.data.allContentfulGallery.edges[newIndex].node
     })
@@ -37,8 +70,8 @@ class IndexPage extends Component {
   render() {
 
     const { post, posts } = this.state
-    //console.log(post);
-    //console.log(posts);
+    console.log(post);
+    console.log(posts);
     return (
       <Layout>
         <div className="App">
@@ -58,7 +91,11 @@ class IndexPage extends Component {
               <h1>Image slideshow React tutorial.</h1>
             </section>
 
-            <Slider post={post} />
+            <Slider
+              post={this.state.post}
+              posts={this.state.posts}
+
+            />
 
           </div>
         </div>
@@ -69,7 +106,7 @@ class IndexPage extends Component {
 
 export const query = graphql`
   query HomeQuery {
-    allContentfulGallery(limit: 8) {
+    allContentfulGallery(limit: 8, sort: { fields:index, order: ASC}) {
       edges {
         node {
           title
