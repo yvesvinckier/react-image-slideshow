165cc3dc29b35b5f5e524b6a6f8571cd9e691e4e
Yves Vinckier <yves.vinckier@live.fr>
2018-12-20 23:56:51 +0100

    prevNext

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import throttle from 'lodash/throttle'
 
 import Slider from '../components/Slider'
 import Layout from '../components/layout'
+// import arrowLeft from '../images/arrow-left'
 
 class IndexPage extends Component {
 
@@ -72,13 +73,27 @@ class IndexPage extends Component {
     return (
       <Layout>
         <main className="site-wrapper">
-          <div className="content content--fixed">
-          </div>
           <div className="content">
             {/* {posts.map(({ node: post }) => (
                   <Slider key={post.id} post={post} />
                 ))} */}
             <Slider key={post.id} post={post} />
+            <div className="prev_next">
+              <button
+                className="to_prev"
+                onClick={() => this.prevPost()}
+                disabled={post.index === 0}>
+                <svg width="12px" height="8px" viewBox="0 0 12 8" version="1.1" xmlns="http://www.w3.org/2000/svg" xlinkHref="http://www.w3.org/1999/xlink">
+                  <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+                    <g id="Artboard-Copy-5" transform="translate(-569.000000, -625.000000)" stroke="#FFFFFF" strokeWidth="2">
+                      <g id="Group-6" transform="translate(570.000000, 623.000000)">
+                        <polyline id="Rectangle-2" transform="translate(4.949747, 8.949747) scale(-1, -1) rotate(-45.000000) translate(-4.949747, -8.949747) " points="8.44974747 12.4497475 1.44974747 12.4497475 1.44974747 5.44974747 1.44974747 5.44974747"></polyline>
+                      </g>
+                    </g>
+                  </g>
+                </svg>
+              </button>
+            </div>
             <button
               onClick={() => this.nextPost()}
               disabled={post.index === posts.length - 1}
@@ -87,6 +102,7 @@ class IndexPage extends Component {
               onClick={() => this.prevPost()}
               disabled={post.index === 0}
             >Prev</button>
+
           </div>
         </main>
       </Layout>
@@ -96,23 +112,23 @@ class IndexPage extends Component {
 
 export const query = graphql`
   query HomeQuery {
-    allContentfulGallery(limit: 8, sort: { fields:index, order: ASC}) {
-      edges {
+          allContentfulGallery(limit: 8, sort: {fields: index, order: ASC}) {
+          edges {
         node {
           title
           id
-          slug
-          index
+        slug
+        index
           cover {
-            title
+          title
             resize (width: 1920, quality: 100) {
-              src
-            }
-          }
+          src
+        }
         }
       }
     }
   }
+}
 `
 
 export default IndexPage
