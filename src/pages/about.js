ebf6b63baf0a3c12079b2b08fe5d192cbc10c0d7
Yves Vinckier <yves.vinckier@live.fr>
2019-04-14 23:37:08 +0200

    setting with react-three-fiber 1.5.3

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,20 +1,76 @@
-import React, { useState, useCallback } from 'react'
-import { Canvas } from 'react-three-fiber'
+import React, { useState, useCallback, useMemo } from 'react'
 import * as THREE from 'three/src/Three'
+import styled from 'styled-components'
+import Layout from '../components/layout'
+
+import { vertexShader, fragmentShader } from '../shaders/XFaderShader'
+
+import { Canvas, useThree } from 'react-three-fiber'
+import { useSpring, animated } from 'react-spring/three'
+
+import '../style.css'
 import data from '../components/data'
+
+const Grid = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  .item{
+    position: relative;
+    width: 100vw;
+    height: 100vw;
+    @media screen and (min-width: ${props => props.theme.responsive.small}) {
+      width: 50vw;
+      height: 50vw;
+    }
+  }
+`
 
 const loader = new THREE.TextureLoader()
 
-function ImageWebgl() {
-  return console.log(loader), null
+function ImageWebgl({ url1, url2, disp, intensity, hovered }) {
+  const { progress } = useSpring({ progress: hovered ? 1 : 0 })
+  const { invalidate } = useThree()
+  const args = useMemo(
+    () => {
+      const texture1 = loader.load(url1, invalidate)
+      const texture2 = loader.load(url2, invalidate)
+      const dispTexture = loader.load(disp, invalidate)
+
+      dispTexture.wrapS = dispTexture.wrapT = THREE.RepeatWrapping
+      texture1.magFilter = texture2.magFilter = THREE.LinearFilter
+      texture1.minFilter = texture2.minFilter = THREE.LinearFilter
+
+      return {
+        uniforms: {
+          effectFactor: { type: 'f', value: intensity },
+          dispFactor: { type: 'f', value: 0 },
+          texture: { type: 't', value: texture1 },
+          texture2: { type: 't', value: texture2 },
+          disp: { type: 't', value: dispTexture }
+        },
+        vertexShader,
+        fragmentShader
+      }
+    },
+    [url1, url2, disp, intensity, invalidate]
+  )
+
+  return (
+    <mesh>
+      <planeBufferGeometry name="geometry" args={[8, 8]} />
+      <animated.shaderMaterial
+        name="material"
+        args={[args]}
+        uniforms-dispFactor-value={progress}
+      />
+    </mesh>
+  )
 }
 
 function Image(props) {
-  // the setHover function update the hovered vlue and the initial value is false
   const [hovered, setHover] = useState(false)
   const hover = useCallback(() => setHover(true), [])
   const unhover = useCallback(() => setHover(false), [])
-
   return (
     <div
       className="item"
@@ -31,23 +87,20 @@ function Image(props) {
       >
         <ImageWebgl {...props} hovered={hovered} />
       </Canvas>
-      <img src={props.url1} />
     </div>
   )
 }
 
-const AboutPage = () => (
-  <div className="grid">
-    {data.map(([url1, url2, disp, intensity], index) => (
-      <Image
-        key={index}
-        url1={url1}
-        url2={url2}
-        disp={disp}
-        intensity={intensity}
-      />
-    ))}
-  </div>
-)
+function AboutPage() {
+  return (
+    <Layout>
+      <Grid>
+        {data.map(([url1, url2, disp, intensity], index) => (
+          <Image key={index} url1={url1} url2={url2} disp={disp} intensity={intensity} />
+        ))}
+      </Grid>
+    </Layout>
+  )
+}
 
 export default AboutPage
