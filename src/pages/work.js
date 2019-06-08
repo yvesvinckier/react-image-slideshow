56ca5b65db0a2334076953c322b999abf9cceb99
Yves Vinckier <yves.vinckier@live.fr>
2019-06-08 23:53:48 +0200

    Card.js refactor animation

--- src/pages/work.js
+++ src/pages/work.js
@@ -10,63 +10,59 @@ const HomeList = styled.ul`
   width: 100%;
   max-width: 1140px;
   margin: 0 auto 2em;
-  padding: 2em 1em 4em;
+  padding: 2em 1em 12em;
   @media screen and (min-width: ${props => props.theme.responsive.small}) {
     display: flex;
     flex-flow: row wrap;
     justify-content: space-between;
   }
   .gatsby-image-wrapper {
-    margin: 1em 1em 1em 1em;
-    height: 500px;
+    height: 250px;
+  }
+  .card {
+    position: relative;
+    flex: 0 0 45%;
+    margin: 0.5vw 0;
+    will-change: transform;
   }
   li {
-    background: white;
-    div {
-      transition: transform 0.5s;
-      height: 100%;
-      object-fit: cover;
-      object-position: 50% 50%;
-    }
-
-    position: relative;
-    flex: 0 0 49.45%;
-    margin: 0.5vw 0;
+    /* background: white; */
   }
   h2 {
-    padding-top: 18px;
-    font-weight: 500;
-    text-align: center;
+    font-weight: 600;
+    text-transform: uppercase;
+    text-align: left;
     line-height: 1.2;
     letter-spacing: 0.02em;
-    font-size: 20px;
+    font-size: 26px;
     color: ${props => props.theme.colors.white};
   }
   h4 {
+    padding-top: 18px;
     font-size: 12px;
     color: ${props => props.theme.colors.grey};
-    font-weight: 300;
+    font-weight: 500;
     line-height: 1.46667;
     letter-spacing: 0.05em;
-    text-align: center;
-    padding-bottom: 65px;
+    text-align: left;
+    padding-bottom: 10px;
   }
 `
 const BlackBackgroung = styled.div`
   position: relative;
-  background: ${props => props.theme.colors.base};;
+  background: ${props => props.theme.colors.base};
   width: 100%;
-  padding-bottom: 60px;
-  > h2{
-        color: ${props => props.theme.colors.white};
-        font-size: 5em;
-        font-weight: 700;
-        padding-top: 120px;
-        padding-left: 120px;
-        text-transform: uppercase;
+  > h2 {
+    margin: 0 auto;
+    max-width: 1140px;
+    color: ${props => props.theme.colors.white};
+    font-size: 4em;
+    font-weight: 700;
+    padding-top: 120px;
+    text-transform: uppercase;
+    padding-bottom: 20px;
   }
 `
-
 
 const WorkPage = ({ data }) => {
   const cards = data.allContentfulGallery.edges
@@ -77,21 +73,19 @@ const WorkPage = ({ data }) => {
         <h2>Projets</h2>
         <HomeList>
           {cards.map(({ node: card }) => (
-
-            <li key={card.id}>
-              <Card>
+            <Card key={card.id}>
+              <li>
                 <Link to={`/${card.slug}/`}>
                   <Img
                     fluid={card.cover.fluid}
                     alt={card.cover.title}
-                    title={card.cover.title}
                     backgroundColor={'#f1f1f1'}
                   />
-                  <h2>{card.title}</h2>
                   <h4>{card.category.name}</h4>
+                  <h2>{card.title}</h2>
                 </Link>
-              </Card>
-            </li>
+              </li>
+            </Card>
           ))}
         </HomeList>
       </BlackBackgroung>
@@ -101,25 +95,29 @@ const WorkPage = ({ data }) => {
 
 export const query = graphql`
   query {
-    allContentfulGallery(limit: 1000, sort: { fields: createdAt, order: DESC }) {
-        edges{
-            node{
-                id
-                slug
-                title
-                cover{
-                    title
-                    fluid(maxWidth: 1800) {
-                    ...GatsbyContentfulFluid_withWebp_noBase64
-                    }
-                }
-                category {
-                    name
-                    categoryslug
-                  }
+    allContentfulGallery(
+      limit: 1000
+      sort: { fields: createdAt, order: DESC }
+    ) {
+      edges {
+        node {
+          id
+          slug
+          title
+          cover {
+            title
+            fluid(maxWidth: 1800) {
+              ...GatsbyContentfulFluid_withWebp_noBase64
             }
+          }
+          category {
+            name
+            categoryslug
+          }
         }
+      }
     }
-  }`
+  }
+`
 
 export default WorkPage
